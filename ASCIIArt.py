c8a1c44226ea58ca0c8d2073984d3a67e60c245a
NaThong <nathongknight@yahoo.com>
2019-04-24 20:19:44 -0700

    test

--- ASCIIArt.py
+++ ASCIIArt.py
@@ -1,7 +1,11 @@
+import numpy as np
+import Pillow
+
 #70 levels of gray
-gscale70 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,"^`'. "
+gscale70 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
 
 #10 levels of gray
 gscale10 = "@%#*+=-:. "
 
+print(gscale70)
 print(gscale10)
