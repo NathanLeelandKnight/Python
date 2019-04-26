1ec2eef8374e228744c902087a0826b32b7279f8
Nathan Knight <nathan.leeland.knight@gmail.com>
2019-04-25 19:39:26 -0700

    test push

--- ASCIIArt.py
+++ ASCIIArt.py
@@ -1,5 +1,7 @@
 import numpy as np
-import Pillow
+import sys, random, argparse
+from PIL import Image
+import math
 
 #70 levels of gray
 gscale70 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
@@ -7,5 +9,9 @@ gscale70 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^
 #10 levels of gray
 gscale10 = "@%#*+=-:. "
 
-print(gscale70)
-print(gscale10)
+if len(sys.argv) != 2:
+	print("Please pass only one image file in as an argument!")
+
+image = Image.open(sys.argv[1]).convert("L")
+
+image.show()
