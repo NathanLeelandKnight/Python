b3fba9c7b7eaba1fdb3e898414ae814fc01c63ec
Nathan Knight <nathan.leeland.knight@gmail.com>
2019-04-29 10:29:44 -0700

    save

--- ASCIIArt.py
+++ ASCIIArt.py
@@ -9,9 +9,32 @@ gscale70 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^
 #10 levels of gray
 gscale10 = "@%#*+=-:. "
 
-if len(sys.argv) != 2:
-	print("Please pass only one image file in as an argument!")
+if len(sys.argv) != 4:
+	print("Use program as: python ASCIIArt.py \'image.jpg\' \'#chars to use (10 or 70)\' \'#columns\'")
+	exit()
+
+scaleNum = int(sys.argv[2])
+cols = int(sys.argv[3])
+
+if 10 != scaleNum != 70:
+	print("#chars to use needs to be 10 or 70")
+	exit()
+
+if not (0 < cols < 251):
+	print("#columns must be between [1, 250]")
+	exit()
 
 image = Image.open(sys.argv[1]).convert("L")
 
-image.show()
+W = image.size[0]
+H = image.size[1]
+
+scale = W / H
+tileW = W / cols
+tileH = tileW / scale
+
+def getAverageL(image):
+	im = np.array(image)
+	w,h = im.shape
+	return np.average(im.reshape(w*h))
+
